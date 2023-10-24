4044c9a925df8bd055a27dff3a84d45aae39dccd
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-10-24 13:40:32 -0400

    Fix call to notifyAll after connection is made

--- src/jhv_request.ts
+++ src/jhv_request.ts
@@ -41,7 +41,7 @@ class JhvRequest {
     };
 
     if (this.connector.connection) {
-      this.connector.notifyAll([this.message], null, registerAndSend);
+      this.connector.connection.notifyAll([this.message], null, registerAndSend);
     } else {
       await registerAndSend();
     }
